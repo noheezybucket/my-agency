c74ea2fd9248fac354a4a2ffb92ad3e116feda2b
noheezybucket <seydinag023@gmail.com>
2024-06-18 02:56:15 +0000

    deploy lamp design

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
     return (
         <div className='flex items-center text-2xl'>
-            <span className='font-extrabold  px-3 py-1'>shift</span>
+            <span className='font-extrabold'>shift</span>
             <span className='font-normal'>agency</span>
         </div>
 
--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -4,16 +4,35 @@ import Logo from '../Logo'
 
 const Footer = () => {
     return (
-        <footer className={cn("dark:bg-bg dark:text-white py-10")}>
-            <div className="mx-auto w-10/12 bg-slate-100 text-bg rounded-lg grid grid-cols-3">
-                <Logo />
+        <footer className={cn("dark:bg-bg dark:text-white py-5 space-y-5")}>
+            <div className="mx-auto w-10/12 bg-slate-100 text-bg rounded-lg grid grid-cols-3 p-4 gap-10">
                 <div>
-                    <span>MENU</span>
+                    <Logo />
+                    <p className='text-justify'>shift est une agence de social media marketing, ses services tournent autour du développement de sites/apps web et mobiles, l'identité visuelle,
+
+                    </p>
                 </div>
 
                 <div>
-                    <span>CONTACTS</span>
+                    <div>LIENS RAPIDES</div>
+                    <ul>
+                        <li>accueil</li>
+                        <li>services</li>
+                        <li>a propos</li>
+                    </ul>
                 </div>
+
+                <div>
+                    <div>RÉSEAUX</div>
+                    <ul className='list-disc pl-4'>
+                        <li>snapchat</li>
+                        <li>instagram</li>
+                        <li>linkedin</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='text-center'>
+                © Tout droits réservés 2024, Shift Agency.
             </div>
         </footer>
     )
